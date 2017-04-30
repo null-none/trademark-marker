eb5549be45799c8a276bdf2544981086e91dc724
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2017-04-30 08:16:29 +0300

    init project

---
+++ trademac/marker.py
@@ -0,0 +1,17 @@
+import urllib, json
+
+class API(object):
+
+    def __init__(self, params):
+        self.url = 'http://www.markerapi.com/api/v1/trademark/search/{0}/username/{1}/password/{2}'
+        self.search = search
+        self.username = username
+        self.password = password
+
+    def result(self):
+        response = urllib.urlopen(self.url.format(urllib.urlencode(self.search),
+                                                  urllib.urlencode(self.username),
+                                                  urllib.urlencode(self.password)))
+        data = response.read()
+        result = json.loads(data)
+        return result
